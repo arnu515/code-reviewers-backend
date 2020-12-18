e9f0b166ba99c3a5415254bf3a71a0400425a97c
arnu515 <arnu5152@gmail.com>
2020-12-18 21:04:33 +0530

    use username to make files unique when storing in digitalocean spaces

--- src/util/files.py
+++ src/util/files.py
@@ -4,13 +4,15 @@ from typing import Tuple, Optional
 from . import security, spaces
 
 
-def create_encrypted_file(filename: str, content: str, folder: str = "code") -> Tuple[str, bool]:
+def create_encrypted_file(filename: str, content: str, folder: str = "code",
+                          username: Optional[str] = None) -> Tuple[str, bool]:
     """
     Creates a file in the filesystem if in dev, or in a digitalocean block volume if in prod
 
     :param filename: Name of the file (no extension needed)
     :param content: Content to put in the file (is encrypted)
     :param folder: Folder to put in in
+    :param username: The username of the owner of the file. Only used when uploading to DigitalOcean Spaces
     :return: Returns a tuple with the first element being the path and the second tells if the file was created locally
     or not
     """
@@ -21,12 +23,24 @@ def create_encrypted_file(filename: str, content: str, folder: str = "code") -> 
             f.write(security.enc(content).encode())
         return pathname, True
     else:
-        pathname = path.join(folder, filename)
+        if username:
+            pathname = path.join(folder, username, filename)
+        else:
+            pathname = path.join(folder, filename)
         spaces.upload_file_from_string(security.enc(content), pathname)
         return pathname, False
 
 
-def get_encrypted_file_contents(filename: str, folder: str = "code", local: bool = True) -> Optional[str]:
+def get_encrypted_file_contents(filename: str, folder: str = "code", local: bool = True,
+                                username: Optional[str] = None) -> Optional[str]:
+    """
+    Gets and decrypts the contents of an encrypted file saved with create_encrypted_file()
+
+    :param filename: The name of the file
+    :param folder: The folder which will have the file
+    :param local: If the file was stored locally or on DigitalOcean Spaces
+    :param username: Username of the file's owner. Only used when storing in DigitalOcean
+    """
     if filename.split(".")[-1] != "encrypted":
         filename += ".encrypted"
     if local:
@@ -37,7 +51,10 @@ def get_encrypted_file_contents(filename: str, folder: str = "code", local: bool
         except FileNotFoundError:
             return None
     else:
-        pathname = path.join(folder, filename)
+        if username:
+            pathname = path.join(folder, username, filename)
+        else:
+            pathname = path.join(folder, filename)
         try:
             return security.dec(spaces.get_file_contents(pathname).decode())
         except Exception as e:
